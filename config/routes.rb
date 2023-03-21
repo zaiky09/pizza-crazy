8037b55a7cec70e6da624609468c802dc5389083
zaiky09 <zamilmozamil@outlook.com>
2023-03-21 07:48:24 +0300

    added ReataurantPizzas route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
   # Pizzas
   resources :pizzas, only: [:index]
-  #Restaurants
+  # Restaurants
   resources :restaurants, only: [:index, :show, :destroy]
+  # RestaurantPizzas
+  resources :restaurant_pizzas, only: [:create]
+
 
 end
