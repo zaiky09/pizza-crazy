eb8427bc0ddb6df0fe2310f3cb0cf6e22179ee4f
zaiky09 <zamilmozamil@outlook.com>
2023-03-21 06:22:01 +0300

    Restaurant model created and db:migrated

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_21_024911) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_21_032110) do
   create_table "pizzas", force: :cascade do |t|
     t.string "name"
     t.string "ingredients"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "restaurants", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
