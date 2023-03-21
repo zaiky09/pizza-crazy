804f7d7e0dd86269ae94e1d593e90b1f9284ea0b
zaiky09 <zamilmozamil@outlook.com>
2023-03-21 07:11:36 +0300

    created routes for pizzas & restaurants

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  # Pizzas
+  resources :pizzas, only; [:index]
+  #Restaurants
+  resources :restaurants, only; [:index, :show, :destroy]
 
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
